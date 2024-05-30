a0cc548a0f46c59a5b23bb47d824d288cffc90a9
manfred <manfred.engler.95@gmail.com>
2024-05-30 14:29:18 -0400

    chore: Agrego test unitarios sencillos a modo de ejemplo con coverage

--- app/fight.py
+++ app/fight.py
@@ -1,3 +1,15 @@
+from errors.fight import EmptySteps
+
 def get_steps(steps:dict):
     """Obtiene un dict con movimientos y golpes y devuelve una lista de tuplas con forma [movimiento, golpe]"""
-    return list(zip(steps.get("movimientos"), steps.get("golpes")))
+    movements= steps.get("movimientos")
+    strikes= steps.get("golpes")
+    if movements is None or strikes is None:
+        raise EmptySteps("Se esperaban ambos diccionarios: movimientos y golpes")
+    
+    if not isinstance(movements, list):
+        raise TypeError("El valor de 'movimientos' debe ser una lista")
+    
+    if not isinstance(strikes, list):
+        raise TypeError("El valor de 'golpes' debe ser una lista")
+    return list(zip(movements, strikes))
