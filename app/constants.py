70a43dd10eb7d32a8017db7e8bc12d0abd09dbfa
manfred <manfred.engler.95@gmail.com>
2024-05-30 12:26:07 -0400

    feat: Agregado archivo constants

---
+++ app/constants.py
@@ -0,0 +1,52 @@
+STARTING_DRAW_LIMIT = 2
+DEFAULT_STARTING_PLAYER = 1
+
+PLAYER_1 = {
+    "number" : 1,
+    "name": "Tonyn Stallone"
+}
+
+PLAYER_2 = {
+    "number" :2,
+    "name": "Arnaldor Shuatseneguer"
+}
+
+HABILITIES = {
+    "Tonyn Stallone": {
+        "DSD+P": {
+            "name": "Taladoken",
+            "damage": 3,
+        },
+        "SD+K": {
+            "name": "Remuyuken",
+            "damage": 2,
+        },
+        "P": {
+            "name": "Puño",
+            "damage": 1,
+        },
+        "K": {
+            "name": "Patada",
+            "damage": 1,
+        }
+    },
+    "Arnaldor Shuatseneguer" : {
+        "SA+K": {
+            "name": "Remuyuken",
+            "damage": 3,
+        },
+        "ASA+P": {
+            "name": "Taladoken",
+            "damage": 2,
+        },
+        "P": {
+            "name": "Puño",
+            "damage": 1,
+        },
+        "K": {
+            "name": "Patada",
+            "damage": 1,
+        }
+    },
+
+}
