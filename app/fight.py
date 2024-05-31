0c600368974127d72f0c6cd423bf9292b129c9fe
manfred <manfred.engler.95@gmail.com>
2024-05-30 20:56:56 -0400

    feat: Agregado zip_steps para "parear" turnos de jugadores, agregados ejemplos en main

--- app/fight.py
+++ app/fight.py
@@ -1,3 +1,4 @@
+from itertools import zip_longest
 from errors.fight import EmptySteps
 
 def get_steps(steps:dict):
@@ -13,3 +14,11 @@ def get_steps(steps:dict):
     if not isinstance(strikes, list):
         raise TypeError("El valor de 'golpes' debe ser una lista")
     return list(zip(movements, strikes))
+
+def zip_steps(input_dict: dict):
+    """Parea cada serie de combinaciones por jugador"""
+    player_1_steps = get_steps(steps=input_dict.get("player1"))
+    player_2_steps = get_steps(steps=input_dict.get("player2"))
+    # El largo puede variar, por ende parea las restantes con un lista por defecto
+    return list(zip_longest(player_1_steps, player_2_steps, fillvalue=["", ""]))
+
--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
 from input_reader import read_input
+from fight import zip_steps
 
-def main():
-    print("Hello Talana!")
-    print(read_input("ejemplo_1"))
+EXAMPLES = [
+    "ejemplo_1", "ejemplo_2_j1", "ejemplo_3_j2"
+]
+
+def main(input_file:str=EXAMPLES[0]):
+    input_dict = read_input(input_file)
+
+    steps = zip_steps(input_dict=input_dict)
 
 if __name__ == "__main__":
-    main()
+    main(input_file=EXAMPLES[0])
